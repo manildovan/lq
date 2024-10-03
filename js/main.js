0e3223b0b1a86796a471b39edd99eb43626b24ed
Manildo Alfredo Nihuta <manildoalfredo@live.com>
2024-10-03 15:26:57 +0200

    d

--- js/main.js
+++ js/main.js
@@ -280,3 +280,38 @@ jQuery(document).ready(function($) {
 	});
 
 });
+
+
+
+  var swiper = new Swiper('.mySwiper', {
+    slidesPerView: 5,  // Quantidade de logos visíveis
+    spaceBetween: 30,  // Espaçamento entre os logos
+    loop: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+      320: {
+        slidesPerView: 2,
+        spaceBetween: 10,
+      },
+      640: {
+        slidesPerView: 3,
+        spaceBetween: 20,
+      },
+      1024: {
+        slidesPerView: 5,
+        spaceBetween: 30,
+      },
+    },
+  });
+
